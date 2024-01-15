2300c3bf453773dd53df380d92d4ee53a4f39122
ovchinikov <sergeyvinokov@gmail.com>
2024-01-15 22:07:26 +0300

    exercise 5.9-5.11

--- blog/controllers/blog.js
+++ blog/controllers/blog.js
@@ -56,7 +56,7 @@ router.post('/', middleware.tokenExtractor, async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', middleware.tokenExtractor, async (req, res) => {
   const blog = await Blog.findById(req.params.id);
   console.log(blog.user._id.toString());
   const decodedToken = jwt.verify(req.token, process.env.SECRET);
@@ -67,25 +67,34 @@ router.delete('/:id', async (req, res) => {
   console.log(user._id.toString());
 
   if (user._id.toString() === blog.user._id.toString()) {
-    await blog.deleteOne();
-    res.json({ message: 'Successfully deleted blog' });
+    await Blog.findByIdAndRemove(req.params.id);
+    res.status(204).end();
   } else {
-    res.sendStatus(403);
+    res.status(401).json({ error: 'unauthorized' });
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', middleware.tokenExtractor, async (req, res) => {
   const { title, author, url, likes } = req.body;
-  const blog = {
+  const updatedBlog = {
     title,
     author,
     url,
     likes,
   };
-  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
-    new: true,
-  });
-  res.json(updatedBlog);
+  const blog = await Blog.findById(req.params.id);
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  if (user._id.toString() === blog.user._id.toString()) {
+    const result = await Blog.findByIdAndUpdate(req.params.id, updatedBlog, {
+      new: true,
+    });
+    res.json(result);
+  }
 });
 
 module.exports = router;
